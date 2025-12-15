6b996a1b4a8072239fcf098f629f01558ed34e12
Mahla <Mahlakalhor82@gmail.com>
2025-12-15 13:49:22 +0330

    pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    
+      <Image src="/Img/main_banner.png" alt="logo" width={220} height={84} />
     </>
   );
 }
